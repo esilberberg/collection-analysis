d524a6495b208a49e23e52f2903a1c820930fc75
Eric Silberberg <72359394+esilberberg@users.noreply.github.com>
2022-06-24 15:43:04 -0400

    documentation

--- ratio_offsite_onsite.py
+++ ratio_offsite_onsite.py
@@ -6,6 +6,7 @@ df = pd.read_csv(csv)
 
 df['Series'] = df['Call Number'].astype(str).str[0:2]
 
+# Organize by series then by sublibrary, then tally it up
 df2 = df.groupby(['Series', 'Sublibrary'])['Sublibrary'].count()
 
 print(df2)
