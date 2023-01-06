2e8b427e9b115b37712ceab80e000210c5af7e5b
Eric Silberberg <72359394+esilberberg@users.noreply.github.com>
2023-01-05 21:32:19 -0500

    added tokenization

--- subjects_analysis.py
+++ subjects_analysis.py
@@ -1,5 +1,8 @@
 import pandas as pd
+import numpy as np
 import string
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
 
 def subject_lookup(topic_num, lookup):
     """Assign a subject to a title based on its LC call number."""
@@ -8,9 +11,28 @@ def subject_lookup(topic_num, lookup):
             return lookup['Subject'][i]
 
 def strip_punctuations(text):
+    """Remove punctuation marks."""
     for char in string.punctuation:
-        text = text.replace(char, "")
+        text = text.replace(char, " ")
     return text
+
+def get_keywords_tally(dataframe):
+    """Build a tokenized list of keywords from book titles in dataframe."""
+    titles = []
+    for title in dataframe['Title']:
+        title = strip_punctuations(title)
+        titles.append(title.lower())
+    
+    stop_words = set(stopwords.words('english'))
+    keywords = []
+    for t in titles:
+        words = word_tokenize(t)
+        for w in words:
+            if w not in stop_words:
+                keywords.append(w)
+
+    return pd.value_counts(np.array(keywords))
+
 
 df_lookup = pd.read_csv('LC-Classification-(L).csv')
 df = pd.read_excel('L-Class_Print_Titles_CLEAN.xlsx')
@@ -29,8 +51,15 @@ for subclass in subclass_list:
 
     df_out = pd.concat([df_out, titles])
 
+
 lc_subclass = df_out.loc[df_out['Subclass'] == 'LC']
+pre_1964 = lc_subclass.loc[lc_subclass['Publication_Date'] <= 1964]
+post_1964 = lc_subclass.loc[(lc_subclass['Publication_Date'] > 1964) & (lc_subclass['Publication_Date'] <= 1975)]
 
-pre_1964 = lc_subclass.loc[lc_subclass['Publication_Date'] < 1964]
+pre_1964_keywords = get_keywords_tally(pre_1964)
+post_1964_keywords = get_keywords_tally(post_1964)
 
-print(pre_1964)
+print('Pre-1964----------------------------------------------')
+print(pre_1964_keywords.head(40))
+print('Post-1964----------------------------------------------')
+print(post_1964_keywords.head(40))
